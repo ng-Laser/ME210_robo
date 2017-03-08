29309939f226b126ac686f4cc833b9705c3e7a4e
ng-Laser <noa.glaser@gmail.com>
2017-03-07 17:09:24 -0800

    added esc , more integration

---
+++ master/ESC_Driver.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+
+Servo firstESC; 
+
+void setupESC() {
+  firstESC.attach(11); 
+}
+void test_ESC() { 
+  while(!Serial.available()){
+    delay(100); 
+  }
+  int  value = Serial.parseInt();    // Parse an Integer from Serial
+  firstESC.writeMicroseconds(value);
+}
+
+void shootESC(int time){
+    firstESC.writeMicroseconds(1000);
+    delay(1000);
+    firstESC.writeMicroseconds(1300);
+    delay(time);
+    firstESC.writeMicroseconds(1000);
+    // doesn't currently stop the ESC, if you want to stop write 1000
+}
